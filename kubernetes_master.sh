74a64b51305aca36f9c27466c0140d9dc5c9f688
Marko Harjula <marko.harjula@gmail.com>
2016-06-10 12:19:08 +0300

    Update kubernetes_master.sh

--- kubernetes_master.sh
+++ kubernetes_master.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-echo "Setup etcd" && \
 if [ -d /lib/systemd ]; then 
+  echo "Setup etcd" && \
   wget -q https://raw.githubusercontent.com/mharj/scripts/master/master/etcd.service -O /lib/systemd/system/etcd.service && \
   chmod 644 /lib/systemd/system/etcd.service && \
   systemctl daemon-reload && \
   echo "alias etcdctl='docker exec etcd /etcdctl'" > /etc/profile.d/etcd.sh && \
-  source /etc/profile.d/etcd.sh
-fi && \
-echo "done";
+  source /etc/profile.d/etcd.sh && \
+  echo "done";
+fi
