cc7cb880469c96818a6c61e47534a9f13870d723
Marko Harjula <marko.harjula@gmail.com>
2016-06-10 11:52:08 +0300

    Update kubernetes_master.sh

--- kubernetes_master.sh
+++ kubernetes_master.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 echo "Setup etcd" && \
 if [ -d /lib/systemd ]; then 
-  echo "setup systemd";
+  wget https://raw.githubusercontent.com/mharj/scripts/master/master/etcd.service -O /lib/systemd/system/etcd.service
+  chmod 644 /lib/systemd/system/etcd.service
+  
 fi && \
 echo "done";
