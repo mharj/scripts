541b48f817d8ea6bb04f331134b180a4b884f635
Marko Harjula <marko.harjula@gmail.com>
2016-06-10 12:34:57 +0300

    Update kubernetes_master.sh

--- kubernetes_master.sh
+++ kubernetes_master.sh
@@ -6,5 +6,6 @@ if [ -d /lib/systemd ]; then
   systemctl daemon-reload && \
   echo "alias etcdctl='docker exec etcd /etcdctl'" > /etc/profile.d/etcd.sh && \
   source /etc/profile.d/etcd.sh && \
+  if [ ! -f /etc/default/etcd ]; then echo -e "#ETCD_LISTEN_PEER_URLS default: \"http://localhost:2380,http://localhost:7001\"\nETCD_LISTEN_PEER_URLS=\n#ETCD_LISTEN_CLIENT_URLS default: \"http://localhost:2379,http://localhost:4001\"\nETCD_LISTEN_CLIENT_URLS=\n" > /etc/default/etcd;fi
   echo "done";
 fi
