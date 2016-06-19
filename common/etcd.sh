cbdce6ae2517a12052948c8fe53396b0db5106b6
Marko Harjula <marko.harjula@gmail.com>
2016-06-19 18:56:47 +0300

    Create etcd.sh

---
+++ common/etcd.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+echo "Build etcd ${ETCD_VERSION}" && \
+if [ -d /opt/etcd ]; then rm -rf /opt/etcd;fi && \
+cd /opt && \
+git clone https://github.com/coreos/etcd && \
+cd /opt/etcd && \
+git checkout tags/${ETCD_VERSION} && \
+echo "FROM golang:1.6-onbuild" > Dockerfile && \
+rm -f .dockerignore && \
+docker build -t coreos/etcd . && \
+mkdir /opt/etcd/bin && \
+docker run -i -v /opt/etcd/bin:/go/src/app/bin --rm coreos/etcd /bin/bash -c "cd /go/src/app && ./build" && \
+install -o root -g root -m 0755 /opt/etcd/bin/etcd /usr/bin/etcd && \
+install -o root -g root -m 0755 /opt/etcd/bin/etcdctl /usr/bin/etcdctl && \
+docker rmi coreos/etcd && \
+cd /opt && \
+rm -rf /opt/etcd && \
+echo "done"
