619960bb84f2b8216391f7d1b37c8d7c2b04d893
Marko Harjula <marko.harjula@gmail.com>
2016-06-19 19:04:21 +0300

    Update kubernetes_master.sh

--- kubernetes_master.sh
+++ kubernetes_master.sh
@@ -3,23 +3,6 @@ export ETCD_VERSION=v2.3.7
 if [ -d /lib/systemd ]; then 
   if [ ! -x /usr/bin/etcdctl ] || [ ! -x /usr/bin/etcd ]; then
     curl -s https://raw.githubusercontent.com/mharj/scripts/master/common/etcd.sh | bash ${ETCD_VERSION}
-#    echo "Build etcd ${ETCD_VERSION}" && \
-#    if [ -d /opt/etcd ]; then rm -rf /opt/etcd;fi && \
-#    cd /opt && \
-#    git clone https://github.com/coreos/etcd && \
-#    cd /opt/etcd && \
-#    git checkout tags/${ETCD_VERSION} && \
-#    echo "FROM golang:1.6-onbuild" > Dockerfile && \
-#    rm -f .dockerignore && \
-#    docker build -t coreos/etcd . && \
-#    mkdir /opt/etcd/bin && \
-#    docker run -i -v /opt/etcd/bin:/go/src/app/bin --rm coreos/etcd /bin/bash -c "cd /go/src/app && ./build" && \
-#    install -o root -g root -m 0755 /opt/etcd/bin/etcd /usr/bin/etcd && \
-#    install -o root -g root -m 0755 /opt/etcd/bin/etcdctl /usr/bin/etcdctl && \
-#    docker rmi coreos/etcd && \
-#    cd /opt && \
-#    rm -rf /opt/etcd && \
-#    echo "done"
   fi
   if [ ! -x /usr/bin/etcdctl ] || [ ! -x /usr/bin/etcd ]; then 
     echo "etcd build failed";
