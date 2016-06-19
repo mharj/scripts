00cc85768c5faae9965f9fc777f6fe00b87ccf8f
Marko Harjula <marko.harjula@gmail.com>
2016-06-19 15:33:30 +0300

    Update kubernetes_master.sh

--- kubernetes_master.sh
+++ kubernetes_master.sh
@@ -17,15 +17,18 @@ ETCD_ADVERTISE_CLIENT_URLS=
 EOF
   fi
   echo "done";
-  echo "Build flannel" && \
-  if [ -d /opt/flannel ]; then rm -rf /opt/flannel;fi && \
-  cd /opt && \
-  git clone https://github.com/coreos/flannel.git && \
-  cd /opt/flannel && \
-  echo "FROM golang:1.6-onbuild" > Dockerfile && \
-  mkdir /opt/flannel/bin && \
-  if [[ "$(docker images -q coreos/flannel 2>/dev/null)" != "" ]]; then docker rmi coreos/flannel;fi && \
-  docker build -t coreos/flannel . && \
-  docker run -i -v /opt/flannel/bin:/go/src/app/bin --rm coreos/flannel /bin/bash -c "cd /go/src/app && ./build" && \
-  echo "done"
+  if [ ! -x /usr/bin/flanneld ]; then 
+    echo "Build flannel" && \
+    if [ -d /opt/flannel ]; then rm -rf /opt/flannel;fi && \
+    cd /opt && \
+    git clone https://github.com/coreos/flannel.git && \
+    cd /opt/flannel && \
+    echo "FROM golang:1.6-onbuild" > Dockerfile && \
+    mkdir /opt/flannel/bin && \
+    if [[ "$(docker images -q coreos/flannel 2>/dev/null)" != "" ]]; then docker rmi coreos/flannel;fi && \
+    docker build -t coreos/flannel . && \
+    docker run -i -v /opt/flannel/bin:/go/src/app/bin --rm coreos/flannel /bin/bash -c "cd /go/src/app && ./build" && \
+    install -o root -g root -m 0755 /opt/flannel/bin/flanneld /usr/bin/flanneld && \
+    echo "done"
+  fi
 fi
