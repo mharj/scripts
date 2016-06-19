b7bf1c1b48e5d2eae402d484d3ff005f372b2989
Marko Harjula <marko.harjula@gmail.com>
2016-06-19 16:57:09 +0300

    Update kubernetes_master.sh

--- kubernetes_master.sh
+++ kubernetes_master.sh
@@ -1,22 +1,5 @@
 #!/bin/bash
 if [ -d /lib/systemd ]; then 
-  echo "Setup etcd" && \
-  wget -q https://raw.githubusercontent.com/mharj/scripts/master/master/etcd.service -O /lib/systemd/system/etcd.service && \
-  chmod 644 /lib/systemd/system/etcd.service && \
-  systemctl daemon-reload && \
-  echo "alias etcdctl='docker exec etcd /etcdctl'" > /etc/profile.d/etcd.sh && \
-  source /etc/profile.d/etcd.sh && \
-  if [ ! -f /etc/default/etcd ]; then 
-    cat << EOF > /etc/default/etcd
-#ETCD_LISTEN_PEER_URLS default: \"http://localhost:2380,http://localhost:7001\"
-ETCD_LISTEN_PEER_URLS=
-#ETCD_LISTEN_CLIENT_URLS default: \"http://localhost:2379,http://localhost:4001\"
-ETCD_LISTEN_CLIENT_URLS=
-#ETCD_ADVERTISE_CLIENT_URLS default: "http://localhost:2379,http://localhost:4001"
-ETCD_ADVERTISE_CLIENT_URLS=
-EOF
-  fi
-  echo "done";
   if [ ! -x /usr/bin/etcdctl ] || [ ! -x /usr/bin/etcd ]; then 
     echo "Build etcd" && \
     if [ -d /opt/etcd ]; then rm -rf /opt/etcd;fi && \
@@ -36,6 +19,21 @@ EOF
     echo "etcd build failed";
     exit;
   fi
+  echo "Setup etcd" && \
+  wget -q https://raw.githubusercontent.com/mharj/scripts/master/master/etcd.service -O /lib/systemd/system/etcd.service && \
+  chmod 644 /lib/systemd/system/etcd.service && \
+  systemctl daemon-reload && \
+  if [ ! -f /etc/default/etcd ]; then 
+    cat << EOF > /etc/default/etcd
+#ETCD_LISTEN_PEER_URLS default: \"http://localhost:2380,http://localhost:7001\"
+ETCD_LISTEN_PEER_URLS=
+#ETCD_LISTEN_CLIENT_URLS default: \"http://localhost:2379,http://localhost:4001\"
+ETCD_LISTEN_CLIENT_URLS=
+#ETCD_ADVERTISE_CLIENT_URLS default: "http://localhost:2379,http://localhost:4001"
+ETCD_ADVERTISE_CLIENT_URLS=
+EOF
+  fi
+  echo "done";  
   if [ ! -x /usr/bin/flanneld ]; then 
     echo "Build flannel" && \
     if [ -d /opt/flannel ]; then rm -rf /opt/flannel;fi && \
