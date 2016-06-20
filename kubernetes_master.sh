870f2537acadc0a7e39b580eb45d39742d75ac89
Marko Harjula <marko.harjula@gmail.com>
2016-06-20 12:09:05 +0300

    Update kubernetes_master.sh

--- kubernetes_master.sh
+++ kubernetes_master.sh
@@ -12,18 +12,8 @@ if [ -d /lib/systemd ]; then
   if [ ! -x /usr/bin/flanneld ]; then
     curl -s https://raw.githubusercontent.com/mharj/scripts/master/common/flanneld.sh | bash -s install ${FLANNELD_VERSION}
   fi
-  echo "flanneld systemd service"
-  if [ ! -x /usr/local/bin/mk-docker-opts.sh ]; then
-    wget -q https://raw.githubusercontent.com/coreos/flannel/master/dist/mk-docker-opts.sh -O /usr/local/bin/mk-docker-opts.sh
-    chmod 755 /usr/local/bin/mk-docker-opts.sh
+  if [ ! -x /usr/bin/flanneld ]; then 
+    echo "flanneld build failed";
+    exit;
   fi
-  wget -q https://raw.githubusercontent.com/mharj/scripts/master/master/flanneld.service -O /lib/systemd/system/flanneld.service && \
-  chmod 644 /lib/systemd/system/flanneld.service && \
-  systemctl daemon-reload && \
-  echo "done"
-  echo "docker systemd setup (wait flanneld.service and options from flanneld startup)" && \
-  mkdir -p /etc/systemd/system/docker.service.d && \
-  wget -q https://raw.githubusercontent.com/mharj/scripts/master/flannel.conf -O /etc/systemd/system/docker.service.d/flannel.conf && \
-  systemctl daemon-reload && \
-  echo "done"
 fi
