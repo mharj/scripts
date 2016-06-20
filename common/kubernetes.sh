b4105185d8ffb3e4fc4430ed5f01f67560f57dfd
Marko Harjula <marko.harjula@gmail.com>
2016-06-20 14:33:05 +0300

    Update kubernetes.sh

--- common/kubernetes.sh
+++ common/kubernetes.sh
@@ -3,16 +3,23 @@ BIN_PATH=/usr/local/bin
 K8S_VERSION=$(curl -sS https://storage.googleapis.com/kubernetes-release/release/stable.txt)
 case "$1" in 
   master)
-    mkdir -p /etc/kubernetes/master
+    if [ "${2}" == "install" ]; then
+      mkdir -p /etc/kubernetes/master;
+      [ ! -f /etc/kubernetes/master/config.conf ] && wget -nv https://raw.githubusercontent.com/kismatic/kubernetes-distro-packages/master/kubernetes/master/etc/kubernetes/master/config.conf -O /etc/kubernetes/master/config.conf
+    fi
     BINS="kube-apiserver kube-controller-manager kube-scheduler kubectl kube-proxy";
     ;;
   node) 
-    mkdir -p /etc/kubernetes/node
+    if [ "${2}" == "install" ]; then
+      mkdir -p /etc/kubernetes/node
+    fi
     BINS="kubelet kubectl"
     ;;
   multi)
-    mkdir -p /etc/kubernetes/master
-    mkdir -p /etc/kubernetes/node
+    if [ "${2}" == "install" ]; then
+      mkdir -p /etc/kubernetes/master
+      mkdir -p /etc/kubernetes/node
+    fi
     BINS="kube-apiserver kube-controller-manager kube-scheduler kubectl kubelet kube-proxy";
     ;;
 esac
@@ -21,8 +28,19 @@ case "$2" in
     echo "${2} kubernetes ${1} (${K8S_VERSION})"
     for i in $BINS
     do
-      wget -nv https://storage.googleapis.com/kubernetes-release/release/${K8S_VERSION}/bin/linux/amd64/$i --no-check-certificate -O ${BIN_PATH}/$i; 
+      wget -nv https://storage.googleapis.com/kubernetes-release/release/${K8S_VERSION}/bin/linux/amd64/$i --no-check-certificate -O ${BIN_PATH}/$i;
+      case $i in 
+        kube-apiserver)
+          if [ -x /bin/systemctl ]; then
+            wget -nv https://raw.githubusercontent.com/kismatic/kubernetes-distro-packages/master/kubernetes/master/services/systemd/kube-apiserver.service -O /lib/systemd/system/kube-apiserver
+          fi
+          if [ ! -f /etc/kubernetes/master/config.conf ]
+          ;;
+      esac
     done
+    if [ -x /bin/systemctl ]; then
+      systemctl daemon-reload
+    fi
     ;;
   remove)
     echo "${2} kubernetes ${1} (${K8S_VERSION})"
