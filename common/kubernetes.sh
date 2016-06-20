fb3848e27f01c46272c20ac3da5474f1cd8858e9
Marko Harjula <marko.harjula@gmail.com>
2016-06-20 14:01:44 +0300

    Update kubernetes.sh

--- common/kubernetes.sh
+++ common/kubernetes.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 K8S_VERSION=$(curl -sS https://storage.googleapis.com/kubernetes-release/release/stable.txt)
 #BINS="kubernetes kube-apiserver kube-controller-manager kube-scheduler kubectl kubecfg kubelet kube-proxy";
-BINS="hyperkube"
+#BINS="hyperkube"
 for i in $BINS
 do
   wget https://storage.googleapis.com/kubernetes-release/release/${K8S_VERSION}/bin/linux/amd64/$i --no-check-certificate -O /usr/local/bin/$i; 
